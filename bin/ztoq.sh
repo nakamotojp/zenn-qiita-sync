d375c321cf1046241a54d1b92e0cb521a38e7fd8
naoki <u429561d@ecs.osaka-u.ac.jp>
2024-10-12 19:18:48 +0900

    ✨ feat: display progress

--- bin/ztoq.sh
+++ bin/ztoq.sh
@@ -5,6 +5,7 @@ FILE=$1
 FILENAME=$(basename $FILE .md)
 if [ $FILE != ./articles/* ]; then
     if [ -e "qiita/public/$FILENAME.md" ]; then
+        echo "delete qiita/public/$FILENAME.md cuz it's not included in articles/"
         rm "qiita/public/$FILENAME.md"
     fi
     exit 0
@@ -12,8 +13,10 @@ fi
 
 if [ ! -e "qiita/public/$FILENAME.md" ] && grep -q "published: true" "$FILE"; then
     cd ./qiita
+    echo "generate qiita/public/$FILENAME.md"
     npx qiita new "$FILENAME"
     cd ../
 fi
 
+echo "convert $FILE to qiita/public/$FILENAME.md"
 ./node_modules/.bin/ts-node scripts/ztoq.tsx "$FILE" "./qiita/public/$FILENAME.md"
